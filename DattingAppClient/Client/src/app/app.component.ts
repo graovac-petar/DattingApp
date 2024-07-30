2a7ecdf62070469a9d99f9718ef49cf2ec0625e2
Petar Graovac <101517125+graovac-petar@users.noreply.github.com>
2024-07-30 15:10:59 +0200

    Added register and login

--- DattingAppClient/Client/src/app/app.component.ts
+++ DattingAppClient/Client/src/app/app.component.ts
@@ -1,24 +1,32 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  title = 'Client';
-  users: any
-
+  private accountService = inject(AccountService);
+  
   ngOnInit() {
-    this.http.get('https://localhost:7163/api/appusers').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    });
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user')
+    if(!userString){
+      return;
+    }
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  
 }
