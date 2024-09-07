2a8298ddb85871a10174362f94fa77e9f9b41e1f
Petar Graovac <101517125+graovac-petar@users.noreply.github.com>
2024-09-07 21:19:20 +0200

    promenjen back tako da ulogovani user prati samo usere kad se logujes

--- AppDating.API/SignalR/PresenceTracker.cs
+++ AppDating.API/SignalR/PresenceTracker.cs
@@ -4,8 +4,9 @@
     {
         private static readonly Dictionary<string, List<string>> OnlineUsers = [];
 
-        public Task UserConnected(string username, string connectionId)
+        public Task<bool> UserConnected(string username, string connectionId)
         {
+            var isOnline = false;
             lock (OnlineUsers)
             {
                 if (OnlineUsers.ContainsKey(username))
@@ -15,26 +16,29 @@
                 else
                 {
                     OnlineUsers.Add(username, [connectionId]);
+                    isOnline = true;
                 }
             }
-            return Task.CompletedTask;
+            return Task.FromResult(isOnline);
         }
 
-        public Task UserDisconnected(string username, string connectionId)
+        public Task<bool> UserDisconnected(string username, string connectionId)
         {
+            var isOffline = false;
             lock (OnlineUsers)
             {
-                if (!OnlineUsers.ContainsKey(username)) return Task.CompletedTask;
+                if (!OnlineUsers.ContainsKey(username)) return Task.FromResult(isOffline);
 
                 OnlineUsers[username].Remove(connectionId);
 
                 if (OnlineUsers[username].Count == 0)
                 {
                     OnlineUsers.Remove(username);
+                    isOffline = true;
                 }
             }
 
-            return Task.CompletedTask;
+            return Task.FromResult(isOffline);
         }
 
         public Task<string[]> GetOnlineUsers()
--- DattingAppClient/Client/src/app/_services/presence.service.ts
+++ DattingAppClient/Client/src/app/_services/presence.service.ts
@@ -27,11 +27,11 @@ export class PresenceService {
     this.hubConnection.start().catch(error => console.log(error));
 
     this.hubConnection.on('UserIsOnline', username => {
-      this.toastr.info(username + ' has connected');
+      this.onlineUsers.update(users => [...users, username]);
     })
 
     this.hubConnection.on('UserIsOffline', username => {
-      this.toastr.warning(username + ' has connected');
+      this.onlineUsers.update(users => users.filter(user => user !== username));
     })
 
     this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
